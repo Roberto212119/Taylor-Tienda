b4f01ec42410ea8be3c484b7c7fe0450ac9864d0
Roberto212119 <rmontero80488@ufide.ac.cr>
2025-09-29 17:51:05 -0600

    Codigo
    se mete el codigo de netbeans

---
+++ src/main/java/monteroTiendaTech/monteroTiendaTech/MonteroTiendaTechApplication.java
@@ -0,0 +1,13 @@
+package monteroTiendaTech.monteroTiendaTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonteroTiendaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonteroTiendaTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/monteroTiendaTech/monteroTiendaTech/MonteroTiendaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package monteroTiendaTech.monteroTiendaTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonteroTiendaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
