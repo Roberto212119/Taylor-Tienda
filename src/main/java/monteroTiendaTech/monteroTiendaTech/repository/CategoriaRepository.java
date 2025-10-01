d30d6652e3b7be145dca552ce137b558f15cca2d
Roberto212119 <rmontero80488@ufide.ac.cr>
2025-09-30 22:14:53 -0600

    segundocommit
    se agregan todos los package y categorias fragmentos y se modifica el pom.xml

---
+++ src/main/java/monteroTiendaTech/monteroTiendaTech/repository/CategoriaRepository.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package monteroTiendaTech.monteroTiendaTech.repository;
+ 
+import monteroTiendaTech.monteroTiendaTech.domain.Categoria;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+ 
+public interface CategoriaRepository extends JpaRepository<Categoria,Integer>{
+    public List<Categoria> findByActivoTrue();
+ 
+}
